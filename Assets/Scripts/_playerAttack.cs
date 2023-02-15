85595d5f28c480b068fd449711081befb9cb4277
Bork114 <mattisderyckere@gmail.com>
2023-02-15 16:29:53 +0100

    Animation

--- Assets/Scripts/_playerAttack.cs
+++ Assets/Scripts/_playerAttack.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class _playerAttack : MonoBehaviour
 {
+    Animator _animator;
 
     [SerializeField]
     public float _maxAttackTime;
 
+    [SerializeField]
+    public GameObject _Player;
+
+    [SerializeField]
+    public string _meshName;
+
     float _attackTime;
     public bool _isAttacking;
+
+    public bool _IsPunching = false;
+    //public bool _IsRunning = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        var temp = FindChildGameObjectByName(_Player, _meshName);
+        _animator = temp.GetComponent<Animator>();
         
     }
 
@@ -32,19 +44,41 @@ public class _playerAttack : MonoBehaviour
         {
             _attackTime += Time.deltaTime;
             _isAttacking = true;
+            _animator.SetBool("IsPunching", true);
+            Debug.Log("Test");
         }
         //If mouse button is released, reset attack time and reset bool
         else if (Input.GetKeyUp(KeyCode.Mouse0))
         {
             _attackTime = 0f;
             _isAttacking = false;
+            _animator.SetBool("IsPunching", false);
         }
         else //If attack time exceeds
         {
             _isAttacking = false;
+            _animator.SetBool("IsPunching", false);
         }
 
         //if(_isAttacking)
         //    print(_isAttacking);
     }
+
+    private GameObject FindChildGameObjectByName(GameObject topParentGameObject, string gameObjectName)
+    {
+        for (int i = 0; i < topParentGameObject.transform.childCount; ++i)
+        {
+
+            if (topParentGameObject.transform.GetChild(i).name == gameObjectName)
+                return topParentGameObject.transform.GetChild(i).gameObject;
+
+            GameObject temp = FindChildGameObjectByName(topParentGameObject.transform.GetChild(i).gameObject, gameObjectName);
+
+            if (temp != null)
+                return temp;
+
+        }
+        return null;
+    }
 }
+
